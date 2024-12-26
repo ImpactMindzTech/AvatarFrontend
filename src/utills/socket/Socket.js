1eb64c2545e728d3db61b20ba20023ebf78a5250
sanjayimpact <sanjay.impactmindz@gmail.com>
2024-12-26 16:28:19 +0530

    sadf

--- src/utills/socket/Socket.js
+++ src/utills/socket/Socket.js
@@ -9,8 +9,8 @@ let userId = getLocalStorage("user")?._id;
 
 
 
-//const socket = io(`https://avatarbackend-4v41.onrender.com/`, {
-const socket = io(`https://api.avatarwalk.com/`, {
+const socket = io(`http://localhost:3000/`, {
+//const socket = io(`https://api.avatarwalk.com/`, {
   withCredentials: true,
   query: {user:userId},
   reconnectionAttempts: 5, 
